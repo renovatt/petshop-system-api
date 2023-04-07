fb46012b0fd3900fbed62a8d5b2b45466e5bd268
Will <wlymes@gmail.com>
2023-04-07 15:18:53 -0300

    feat: express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import { router } from './router';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 app.listen(3333, () => console.log('server listening on port 3333'))
